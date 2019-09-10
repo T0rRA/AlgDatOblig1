818de0d3cf6085137c59b5c99b2a51f8e54d3f82
Benjamin <ben.overgaard@gmail.com>
2019-09-10 15:13:48 +0200

    Nesten ferdig oppgave 3, feiler når det er mange like tall i arrayet

--- Oblig1/src/no/oslomet/cs/algdat/Oblig1/Oblig1.java
+++ Oblig1/src/no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -61,17 +61,18 @@ public class Oblig1 {
         int antallForskjelligeVerdier = a.length;
         int tellerTemp = -1;
 
-        if(a.length == 0){
-            return 0;
-        }
         for(int i = 0; i < a.length; i++){
-            for(int j = 0; j < a.length; j++){
+            for(int j = i; j < a.length; j++){
                 if(a[i] == a[j]){
                     tellerTemp++;
                 }
             }
             antallForskjelligeVerdier = antallForskjelligeVerdier - tellerTemp;
+            tellerTemp = -1;
         }
+        System.out.println(antallForskjelligeVerdier);
+
+        return antallForskjelligeVerdier;
     }
 
     ///// Oppgave 4 //////////////////////////////////////
