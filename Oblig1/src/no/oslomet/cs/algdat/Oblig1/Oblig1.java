50448f6ea1a2c85af239190dc118e0ef9e27c9b3
T0rRA <tor.ryan.andersen@gmail.com>
2019-09-10 11:14:43 +0200

    Virker som det virker

---
+++ Oblig1/src/no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -0,0 +1,81 @@
+package no.oslomet.cs.algdat.Oblig1;
+
+////// Løsningsforslag Oblig 1 - 2019 ////////////////////////
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+
+public class Oblig1 {
+    private Oblig1() {
+    }
+
+    ///// Oppgave 1 //////////////////////////////////////
+    public static int maks(int[] a) {
+        throw new NotImplementedException();
+    }
+
+    public static int ombyttinger(int[] a) {
+        throw new NotImplementedException();
+    }
+
+    ///// Oppgave 2 //////////////////////////////////////
+    public static int antallUlikeSortert(int[] a) {
+        throw new NotImplementedException();
+    }
+
+
+    ///// Oppgave 3 //////////////////////////////////////
+    public static int antallUlikeUsortert(int[] a) {
+        throw new NotImplementedException();
+    }
+
+    ///// Oppgave 4 //////////////////////////////////////
+    public static void delsortering(int[] a) {
+        throw new NotImplementedException();
+    }
+
+    ///// Oppgave 5 //////////////////////////////////////
+    public static void rotasjon(char[] a) {
+        throw new NotImplementedException();
+    }
+
+    ///// Oppgave 6 //////////////////////////////////////
+    public static void rotasjon(char[] a, int k) {
+        throw new NotImplementedException();
+    }
+
+    ///// Oppgave 7 //////////////////////////////////////
+    /// 7a)
+    public static String flett(String s, String t) {
+        throw new NotImplementedException();
+    }
+
+    /// 7b)
+    public static String flett(String... s) {
+        throw new NotImplementedException();
+    }
+
+    ///// Oppgave 8 //////////////////////////////////////
+    public static int[] indekssortering(int[] a) {
+        throw new NotImplementedException();
+    }
+
+
+    ///// Oppgave 9 //////////////////////////////////////
+    public static int[] tredjeMin(int[] a) {
+        throw new NotImplementedException();
+    }
+
+    ///// Oppgave 10 //////////////////////////////////////
+    public static int bokstavNr(char bokstav) {
+        throw new NotImplementedException();
+    }
+
+    public static boolean inneholdt(String a, String b) {
+        throw new NotImplementedException();
+    }
+
+}  // Oblig1
---
+++ Oblig1/src/no/oslomet/cs/algdat/Oblig1/tester.java
@@ -0,0 +1,4 @@
+package no.oslomet.cs.algdat.Oblig1;
+
+public class tester {
+}
